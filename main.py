c9894646c5c83f496a105fdc43d1b443dc31eddc
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-26 20:38:55 +0200

    continuation

--- main.py
+++ main.py
@@ -17,9 +17,19 @@ while True:
         first_frame = gray_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-    cv2.imshow("My video", delta_frame)
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
+    cv2.imshow("My video", dil_frame)
 
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h, = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        cv2.imshow("Video", frame)
 
     key = cv2.waitKey(1)
 
